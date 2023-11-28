b7827fe23188da15eaac83474203b97658ac3de1
Peter <peter@Peters-MacBook-Pro.local>
2023-11-28 19:27:08 +0000

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello');
+const generateRandomMessage = () => {
+
+let part1 = ['He', 'She', 'It']
+let part2 = ['is', 'was', 'will be']
+let part3 = ['good', 'bad', 'normal']
+
+return `${part1[Math.floor(Math.random() * 3)]} ${part2[Math.floor(Math.random() * 3)]} ${part3[Math.floor(Math.random() * 3)]}`
+
+}
+
+console.log(generateRandomMessage());
